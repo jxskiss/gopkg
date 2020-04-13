ecad956494b3608cc119279cddf17d48e780462a
jxskiss <jxskiss@126.com>
2020-04-13 23:43:35 +0800

    update http DoRequest to be more flexible
    Change-Id: I2a75d7e8ff50132811f3167d033041da3485a27d

--- easy/http.go
+++ easy/http.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"context"
 	"encoding/xml"
+	"fmt"
 	"github.com/jxskiss/gopkg/json"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
 	"regexp"
 	"strings"
 	"time"
@@ -17,6 +19,9 @@ import (
 
 var (
 	hdrContentTypeKey = http.CanonicalHeaderKey("Content-Type")
+	contentTypeJSON   = "application/json"
+	contentTypeXML    = "application/xml"
+	contentTypeForm   = "application/x-www-form-urlencoded"
 
 	jsonCheck = regexp.MustCompile(`(?i:(application|text)/(json|.*\+json|json\-.*)(;|$))`)
 	xmlCheck  = regexp.MustCompile(`(?i:(application|text)/(xml|.*\+xml)(;|$))`)
@@ -90,17 +95,36 @@ func IsXMLType(contentType string) bool {
 
 // Request represents a request and options to send with the DoRequest function.
 type Request struct {
-	Req  *http.Request
-	Resp interface{}
+	// Req should be a fully prepared http Request to sent, if not nil,
+	// the following `URL`, `Method`, `JSON`, `XML`, `Form`, `Body`
+	// will be ignored.
+	Req *http.Request
 
-	Context   context.Context
-	Timeout   time.Duration
-	Client    *http.Client
+	// If Req is nil, it will be filled using the following data `URL`,
+	// `Method`, `JSON`, `XML`, `Form`, `Body` to construct the `http.Request`.
+	//
+	// For building http body, the priority is JSON > XML > Form > Body.
+	URL    string
+	Method string
+	JSON   interface{}
+	XML    interface{}
+	Form   interface{}
+	Body   interface{}
+
+	// Headers will be copied to the request before sent.
+	Headers map[string]string
+
+	Resp      interface{}
 	Unmarshal func([]byte, interface{}) error
+
+	Context context.Context
+	Timeout time.Duration
+	Client  *http.Client
 
 	DisableRedirect bool
 	DumpRequest     bool
 	DumpResponse    bool
+	RaiseForStatus  bool
 }
 
 func (p *Request) buildClient() *http.Client {
@@ -120,6 +144,148 @@ func (p *Request) buildClient() *http.Client {
 	return &client
 }
 
+func (p *Request) prepareRequest(method string) (err error) {
+	if p.Req != nil {
+		return nil
+	}
+	if method == "" {
+		method = p.Method
+	}
+	if method == "GET" {
+		p.Req, err = http.NewRequest(method, p.URL, nil)
+		return
+	}
+
+	// make JSON body
+	if p.JSON != nil {
+		body, err := p.makeBody(p.JSON, json.Marshal)
+		if err != nil {
+			return err
+		}
+		p.Req, err = http.NewRequest(method, p.URL, body)
+		if err != nil {
+			return err
+		}
+		p.Req.Header.Set(hdrContentTypeKey, contentTypeJSON)
+		return nil
+	}
+
+	// make XML body
+	if p.XML != nil {
+		body, err := p.makeBody(p.XML, xml.Marshal)
+		if err != nil {
+			return err
+		}
+		p.Req, err = http.NewRequest(method, p.URL, body)
+		if err != nil {
+			return err
+		}
+		p.Req.Header.Set(hdrContentTypeKey, contentTypeXML)
+		return nil
+	}
+
+	// make FORM body
+	if p.Form != nil {
+		body, err := p.makeBody(p.Form, marshalForm)
+		if err != nil {
+			return err
+		}
+		p.Req, err = http.NewRequest(method, p.URL, body)
+		if err != nil {
+			return err
+		}
+		p.Req.Header.Set(hdrContentTypeKey, contentTypeForm)
+		return nil
+	}
+
+	// guess the Body data
+	if p.Body != nil {
+		var bodyBuf []byte
+		switch data := p.Body.(type) {
+		case io.Reader:
+			bodyBuf, err = ioutil.ReadAll(data)
+			if err != nil {
+				return err
+			}
+		case []byte:
+			bodyBuf = data
+		case string:
+			bodyBuf = ToBytes_(data)
+		default:
+			err = fmt.Errorf("unsupported body data type: %T", data)
+			return err
+		}
+		body := bytes.NewReader(bodyBuf)
+		p.Req, err = http.NewRequest(method, p.URL, body)
+		if err != nil {
+			return err
+		}
+		contentType := http.DetectContentType(bodyBuf)
+		if contentType != "" {
+			p.Req.Header.Set(hdrContentTypeKey, contentType)
+		}
+		return nil
+	}
+
+	// no body data
+	p.Req, err = http.NewRequest(method, p.URL, nil)
+	if err != nil {
+		return err
+	}
+	return
+}
+
+func marshalForm(v interface{}) ([]byte, error) {
+	var form url.Values
+	switch data := v.(type) {
+	case url.Values:
+		form = data
+	case map[string][]string:
+		form = data
+	case map[string]string:
+		form = make(url.Values, len(data))
+		for k, v := range data {
+			form[k] = []string{v}
+		}
+	}
+	if form == nil {
+		err := fmt.Errorf("unsupported form data type: %T", v)
+		return nil, err
+	}
+	buf := ToBytes_(form.Encode())
+	return buf, nil
+}
+
+type marshalFunc func(interface{}) ([]byte, error)
+
+func (p *Request) makeBody(data interface{}, marshal marshalFunc) (io.Reader, error) {
+	var body io.Reader
+	switch x := data.(type) {
+	case io.Reader:
+		body = x
+	case []byte:
+		body = bytes.NewReader(x)
+	case string:
+		body = strings.NewReader(x)
+	default:
+		buf, err := marshal(data)
+		if err != nil {
+			return nil, err
+		}
+		body = bytes.NewReader(buf)
+	}
+	return body, nil
+}
+
+func (p *Request) prepareHeaders() {
+	if p.Req == nil {
+		return
+	}
+	for k, v := range p.Headers {
+		p.Req.Header.Set(k, v)
+	}
+}
+
 // DoRequest is a convenient function to send request and control redirect
 // and debug options. If `Request.Resp` is provided, it will be used as
 // destination to try to unmarshal the response body.
@@ -129,6 +295,11 @@ func (p *Request) buildClient() *http.Client {
 // For more powerful controls of a http request and convenient utilities,
 // one may take a look at the awesome package `https://github.com/go-resty/resty/`.
 func DoRequest(req *Request) (respContent []byte, status int, err error) {
+	if err = req.prepareRequest(""); err != nil {
+		return
+	}
+	req.prepareHeaders()
+
 	httpReq := req.Req
 	if req.Context != nil {
 		httpReq = httpReq.WithContext(req.Context)
@@ -144,7 +315,7 @@ func DoRequest(req *Request) (respContent []byte, status int, err error) {
 		if err != nil {
 			return
 		}
-		log.Printf("dump request: %v\n", dump.String_())
+		log.Printf("dump http request:\n%s", dump.String_())
 	}
 
 	httpClient := req.buildClient()
@@ -161,12 +332,18 @@ func DoRequest(req *Request) (respContent []byte, status int, err error) {
 		if err != nil {
 			return
 		}
-		log.Printf("dump response: %v\n", dump.String_())
+		log.Printf("dump http response:\n%s", dump.String_())
 	}
 
 	respContent, err = ioutil.ReadAll(httpResp.Body)
 	if err != nil {
 		return
+	}
+	if req.RaiseForStatus {
+		if httpResp.StatusCode >= 400 {
+			err = fmt.Errorf("unexpected status: %v", httpResp.Status)
+			return
+		}
 	}
 
 	if req.Resp != nil && len(respContent) > 0 {
