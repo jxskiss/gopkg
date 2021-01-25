6765b00e5fc8047386c3952a0dbd117716d6dbb9
jxskiss <jxskiss@126.com>
2021-01-25 18:40:52 +0800

    sqlutil: lazy binary wrapper
    Change-Id: Ie481a493476777f94d897344fcccf6077c16d803

--- sqlutil/types_test.go
+++ sqlutil/types_test.go
@@ -3,6 +3,7 @@ package sqlutil
 import (
 	"database/sql"
 	"database/sql/driver"
+	"github.com/jxskiss/gopkg/serialize"
 	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
@@ -60,4 +61,63 @@ func TestTypes(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, tc.a, reflect.ValueOf(tc.b).Elem().Interface())
 	}
+}
+
+type Record struct {
+	ID      int64      `json:"id" db:"db"`           // bigint unsigned not null primary_key
+	Column1 string     `json:"column1" db:"column1"` // varchar(32) not null
+	Extra   LazyBinary `json:"extra" db:"extra"`     // blob
+}
+
+func (p *Record) GetExtra() (map[string]string, error) {
+	unmarshaler := func(b []byte) (interface{}, error) {
+		var out = map[string]string{}
+		var err error
+		if len(b) > 0 {
+			err = (*serialize.StringMap)(&out).UnmarshalProto(b)
+		}
+		return out, err
+	}
+
+	extra, err := p.Extra.Get(unmarshaler)
+	if err != nil {
+		return nil, err
+	}
+	return extra.(map[string]string), nil
+}
+
+func (p *Record) SetExtra(extra map[string]string) {
+	buf, _ := serialize.StringMap(extra).MarshalProto()
+	p.Extra.Set(buf, extra)
+}
+
+func TestLazyBinary(t *testing.T) {
+	extra := map[string]string{
+		"a": "123",
+		"b": "234",
+		"c": "345",
+	}
+	extraBuf, _ := serialize.StringMap(extra).MarshalProto()
+
+	row := &Record{}
+	assert.Len(t, row.Extra.GetBytes(), 0)
+	_, err := row.GetExtra()
+	assert.Nil(t, err)
+
+	row.Extra.Set(extraBuf, nil)
+	assert.Len(t, row.Extra.GetBytes(), len(extraBuf))
+
+	got1, _ := row.GetExtra()
+	got2, _ := row.GetExtra()
+	assert.Equal(t, extra, got1)
+	assert.Equal(t, extra, got2)
+
+	delete(extra, "a")
+	row.SetExtra(extra)
+	got3, _ := row.GetExtra()
+	assert.Equal(t, extra, got3)
+
+	row.Extra.Set(extraBuf, nil)
+	got4, _ := row.GetExtra()
+	assert.Len(t, got4, 3)
 }
