ae8497bfc9e666ff6bc9e6fa946f897692937151
jxskiss <jxskiss@126.com>
2021-05-13 12:59:36 +0800

    more ptr converter functions
    Change-Id: I4d4956845c16d06ae7d2eac51e80f0e897593d74

--- ptr/convert.go
+++ ptr/convert.go
@@ -2,15 +2,6 @@ package ptr
 
 import "strconv"
 
-// IntpToString converts x to a string.
-// It returns an empty string if x is nil.
-func IntpToString(x *int) string {
-	if x == nil {
-		return ""
-	}
-	return strconv.FormatInt(int64(*x), 10)
-}
-
 // IntToStringp converts x to a string pointer.
 func IntToStringp(x int) *string {
 	str := strconv.FormatInt(int64(x), 10)
@@ -26,13 +17,37 @@ func IntpToStringp(x *int) *string {
 	return IntToStringp(*x)
 }
 
-// Int64pToString converts x to a string.
+// IntpToString converts x to a string.
 // It returns an empty string if x is nil.
-func Int64pToString(x *int64) string {
+func IntpToString(x *int) string {
 	if x == nil {
 		return ""
 	}
-	return strconv.FormatInt(*x, 10)
+	return strconv.FormatInt(int64(*x), 10)
+}
+
+// Int32ToStringp converts x to a string pointer.
+func Int32ToStringp(x int32) *string {
+	str := strconv.FormatInt(int64(x), 10)
+	return &str
+}
+
+// Int32pToStringp converts x to a string pointer.
+// It returns nil if x is nil.
+func Int32pToStringp(x *int32) *string {
+	if x == nil {
+		return nil
+	}
+	return Int32ToStringp(*x)
+}
+
+// Int32pToString converts x to a string.
+// It returns an emtpy string if x is nil.
+func Int32pToString(x *int32) string {
+	if x == nil {
+		return ""
+	}
+	return strconv.FormatInt(int64(*x), 10)
 }
 
 // Int64ToStringp converts x to a string pointer.
@@ -50,14 +65,13 @@ func Int64pToStringp(x *int64) *string {
 	return Int64ToStringp(*x)
 }
 
-// StringpToInt converts x to an integer.
-// It returns zero if x is nil or not a valid number string.
-func StringpToInt(x *string) int {
+// Int64pToString converts x to a string.
+// It returns an empty string if x is nil.
+func Int64pToString(x *int64) string {
 	if x == nil {
-		return 0
+		return ""
 	}
-	i, _ := strconv.ParseInt(*x, 0, 0)
-	return int(i)
+	return strconv.FormatInt(*x, 10)
 }
 
 // StringToIntp converts x to an int pointer.
@@ -80,14 +94,44 @@ func StringpToIntp(x *string) *int {
 	return StringToIntp(*x)
 }
 
-// StringpToInt64 converts x to an int64 value.
+// StringpToInt converts x to an integer.
 // It returns zero if x is nil or not a valid number string.
-func StringpToInt64(x *string) int64 {
+func StringpToInt(x *string) int {
 	if x == nil {
 		return 0
 	}
 	i, _ := strconv.ParseInt(*x, 0, 0)
-	return i
+	return int(i)
+}
+
+// StringToInt32p converts x to an int32 pointer.
+// It returns nil if x is not a valid number string.
+func StringToInt32p(x string) *int32 {
+	i, err := strconv.ParseInt(x, 0, 0)
+	if err != nil {
+		return nil
+	}
+	ii := int32(i)
+	return &ii
+}
+
+// StringpToInt32p converts x to an int32 pointer.
+// It returns nil if x is nil or not a valid number string.
+func StringpToInt32p(x *string) *int32 {
+	if x == nil {
+		return nil
+	}
+	return StringToInt32p(*x)
+}
+
+// StringpToInt32 converts x to an integer.
+// It returns zero if x is nil or not a valid number string.
+func StringpToInt32(x *string) int32 {
+	if x == nil {
+		return 0
+	}
+	i, _ := strconv.ParseInt(*x, 0, 0)
+	return int32(i)
 }
 
 // StringToInt64p converts x to an int64 pointer.
@@ -107,4 +151,14 @@ func StringpToInt64p(x *string) *int64 {
 		return nil
 	}
 	return StringToInt64p(*x)
+}
+
+// StringpToInt64 converts x to an int64 value.
+// It returns zero if x is nil or not a valid number string.
+func StringpToInt64(x *string) int64 {
+	if x == nil {
+		return 0
+	}
+	i, _ := strconv.ParseInt(*x, 0, 0)
+	return i
 }
