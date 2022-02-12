f207b5150cfac2ef7152243f8cedb9236d503cd0
jxskiss <jxskiss@126.com>
2022-02-12 16:59:38 +0800

    implement generics LRU
    Change-Id: I8366a20490d74367f1a525318c8c7d2dc6f1ee07

--- lru/cache.go
+++ lru/cache.go
@@ -1,7 +1,6 @@
 package lru
 
 import (
-	"reflect"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -15,14 +14,14 @@ const maxCapacity = 1<<32 - 1
 // will be reused and won't be freed for the lifetime of the cache.
 //
 // Param capacity must be smaller than 2^32, else it will panic.
-func NewCache(capacity int) *Cache {
+func NewCache[K comparable, V any](capacity int) *Cache[K, V] {
 	if capacity > maxCapacity {
 		panic("invalid too large capacity")
 	}
 	list := newList(capacity)
-	c := &Cache{
+	c := &Cache[K, V]{
 		list: list,
-		m:    make(map[interface{}]uint32, capacity),
+		m:    make(map[K]uint32, capacity),
 		buf:  unsafe.Pointer(newWalBuf()),
 	}
 	return c
@@ -32,22 +31,22 @@ func NewCache(capacity int) *Cache {
 //
 // It implements Interface in this package, see Interface for detailed
 // api documents.
-type Cache struct {
+type Cache[K comparable, V any] struct {
 	mu   sync.RWMutex
 	list *list
-	m    map[interface{}]uint32
+	m    map[K]uint32
 
 	buf unsafe.Pointer // *walbuf
 }
 
-func (c *Cache) Len() (n int) {
+func (c *Cache[K, V]) Len() (n int) {
 	c.mu.RLock()
 	n = len(c.m)
 	c.mu.RUnlock()
 	return
 }
 
-func (c *Cache) Has(key interface{}) (exists, expired bool) {
+func (c *Cache[K, V]) Has(key K) (exists, expired bool) {
 	c.mu.RLock()
 	_, elem, exists := c.get(key)
 	if exists {
@@ -57,11 +56,11 @@ func (c *Cache) Has(key interface{}) (exists, expired bool) {
 	return
 }
 
-func (c *Cache) Get(key interface{}) (v interface{}, exists, expired bool) {
+func (c *Cache[K, V]) Get(key K) (v V, exists, expired bool) {
 	c.mu.RLock()
 	idx, elem, exists := c.get(key)
 	if exists {
-		v = elem.value
+		v = elem.value.(V)
 		expired = elem.expires > 0 && elem.expires < time.Now().UnixNano()
 		c.promote(idx)
 	}
@@ -69,11 +68,11 @@ func (c *Cache) Get(key interface{}) (v interface{}, exists, expired bool) {
 	return
 }
 
-func (c *Cache) GetWithTTL(key interface{}) (v interface{}, exists bool, ttl *time.Duration) {
+func (c *Cache[K, V]) GetWithTTL(key K) (v V, exists bool, ttl *time.Duration) {
 	c.mu.RLock()
 	idx, elem, exists := c.get(key)
 	if exists {
-		v = elem.value
+		v = elem.value.(V)
 		if elem.expires > 0 {
 			x := time.Duration(elem.expires - time.Now().UnixNano())
 			ttl = &x
@@ -84,24 +83,24 @@ func (c *Cache) GetWithTTL(key interface{}) (v interface{}, exists bool, ttl *ti
 	return
 }
 
-func (c *Cache) GetQuiet(key interface{}) (v interface{}, exists, expired bool) {
+func (c *Cache[K, V]) GetQuiet(key K) (v V, exists, expired bool) {
 	c.mu.RLock()
 	_, elem, exists := c.get(key)
 	if exists {
-		v = elem.value
+		v = elem.value.(V)
 		expired = elem.expires > 0 && elem.expires < time.Now().UnixNano()
 	}
 	c.mu.RUnlock()
 	return
 }
 
-func (c *Cache) GetNotStale(key interface{}) (v interface{}, exists bool) {
+func (c *Cache[K, V]) GetNotStale(key K) (v V, exists bool) {
 	c.mu.RLock()
 	idx, elem, exists := c.get(key)
 	if exists {
 		expired := elem.expires > 0 && elem.expires < time.Now().UnixNano()
 		if !expired {
-			v = elem.value
+			v = elem.value.(V)
 			c.promote(idx)
 		} else {
 			exists = false
@@ -111,7 +110,7 @@ func (c *Cache) GetNotStale(key interface{}) (v interface{}, exists bool) {
 	return
 }
 
-func (c *Cache) get(key interface{}) (idx uint32, elem *element, exists bool) {
+func (c *Cache[K, V]) get(key K) (idx uint32, elem *element, exists bool) {
 	idx, exists = c.m[key]
 	if exists {
 		elem = c.list.get(idx)
@@ -119,18 +118,18 @@ func (c *Cache) get(key interface{}) (idx uint32, elem *element, exists bool) {
 	return
 }
 
-func (c *Cache) MGet(keys ...interface{}) map[interface{}]interface{} {
+func (c *Cache[K, V]) MGet(keys ...K) map[K]V {
 	nowNano := time.Now().UnixNano()
 	return c.mget(false, nowNano, keys...)
 }
 
-func (c *Cache) MGetNotStale(keys ...interface{}) map[interface{}]interface{} {
+func (c *Cache[K, V]) MGetNotStale(keys ...K) map[K]V {
 	nowNano := time.Now().UnixNano()
 	return c.mget(true, nowNano, keys...)
 }
 
-func (c *Cache) mget(notStale bool, nowNano int64, keys ...interface{}) map[interface{}]interface{} {
-	res := make(map[interface{}]interface{}, len(keys))
+func (c *Cache[K, V]) mget(notStale bool, nowNano int64, keys ...K) map[K]V {
+	res := make(map[K]V, len(keys))
 	c.mu.RLock()
 	for _, key := range keys {
 		idx, elem, exists := c.get(key)
@@ -141,7 +140,7 @@ func (c *Cache) mget(notStale bool, nowNano int64, keys ...interface{}) map[inte
 					continue
 				}
 			}
-			res[key] = elem.value
+			res[key] = elem.value.(V)
 			c.promote(idx)
 		}
 	}
@@ -149,127 +148,79 @@ func (c *Cache) mget(notStale bool, nowNano int64, keys ...interface{}) map[inte
 	return res
 }
 
-func (c *Cache) MGetInt(keys ...int) map[int]interface{} {
-	nowNano := time.Now().UnixNano()
-	return c.mgetInt(false, nowNano, keys...)
+func (c *Cache[K, V]) Set(key K, value V, ttl time.Duration) {
+	var expires int64
+	if ttl > 0 {
+		expires = time.Now().UnixNano() + int64(ttl)
+	}
+	c.mu.Lock()
+	c.checkAndFlushBuf()
+	c.set(key, value, expires)
+	c.mu.Unlock()
 }
 
-func (c *Cache) MGetIntNotStale(keys ...int) map[int]interface{} {
-	nowNano := time.Now().UnixNano()
-	return c.mgetInt(true, nowNano, keys...)
+func (c *Cache[K, V]) MSet(kvmap map[K]V, ttl time.Duration) {
+	var expires int64
+	if ttl > 0 {
+		expires = time.Now().UnixNano() + int64(ttl)
+	}
+
+	c.mu.Lock()
+	c.checkAndFlushBuf()
+	for key, val := range kvmap {
+		c.set(key, val, expires)
+	}
+	c.mu.Unlock()
 }
 
-func (c *Cache) mgetInt(notStale bool, nowNano int64, keys ...int) map[int]interface{} {
-	res := make(map[int]interface{}, len(keys))
-	c.mu.RLock()
+func (c *Cache[K, V]) set(k K, v V, expires int64) {
+	idx, exists := c.m[k]
+	if exists {
+		e := c.list.get(idx)
+		e.value = v
+		e.expires = expires
+		c.list.MoveToFront(e)
+	} else {
+		e := c.list.Back()
+		if e.key != nil {
+			delete(c.m, e.key.(K))
+		}
+		e.key = k
+		e.value = v
+		e.expires = expires
+		c.m[k] = e.index
+		c.list.MoveToFront(e)
+	}
+}
+
+func (c *Cache[K, V]) Delete(key K) {
+	c.mu.Lock()
+	c.checkAndFlushBuf()
+	c.del(key)
+	c.mu.Unlock()
+}
+
+func (c *Cache[K, V]) MDelete(keys ...K) {
+	c.mu.Lock()
+	c.checkAndFlushBuf()
 	for _, key := range keys {
-		idx, elem, exists := c.get(key)
-		if exists {
-			if notStale {
-				expired := elem.expires > 0 && elem.expires < nowNano
-				if expired {
-					continue
-				}
-			}
-			res[key] = elem.value
-			c.promote(idx)
-		}
+		c.del(key)
 	}
-	c.mu.RUnlock()
-	return res
+	c.mu.Unlock()
 }
 
-func (c *Cache) MGetInt64(keys ...int64) map[int64]interface{} {
-	nowNano := time.Now().UnixNano()
-	return c.mgetInt64(false, nowNano, keys...)
-}
-
-func (c *Cache) MGetInt64NotStale(keys ...int64) map[int64]interface{} {
-	nowNano := time.Now().UnixNano()
-	return c.mgetInt64(true, nowNano, keys...)
-}
-
-func (c *Cache) mgetInt64(notStale bool, nowNano int64, keys ...int64) map[int64]interface{} {
-	res := make(map[int64]interface{}, len(keys))
-	c.mu.RLock()
-	for _, key := range keys {
-		idx, elem, exists := c.get(key)
-		if exists {
-			if notStale {
-				expired := elem.expires > 0 && elem.expires < nowNano
-				if expired {
-					continue
-				}
-			}
-			res[key] = elem.value
-			c.promote(idx)
-		}
+func (c *Cache[K, V]) del(key K) {
+	idx, exists := c.m[key]
+	if exists {
+		delete(c.m, key)
+		elem := c.list.get(idx)
+		elem.key = nil
+		elem.value = nil
+		c.list.MoveToBack(elem)
 	}
-	c.mu.RUnlock()
-	return res
 }
 
-func (c *Cache) MGetUint64(keys ...uint64) map[uint64]interface{} {
-	nowNano := time.Now().UnixNano()
-	return c.mgetUint64(false, nowNano, keys...)
-}
-
-func (c *Cache) MGetUint64NotStale(keys ...uint64) map[uint64]interface{} {
-	nowNano := time.Now().UnixNano()
-	return c.mgetUint64(true, nowNano, keys...)
-}
-
-func (c *Cache) mgetUint64(notStale bool, nowNano int64, keys ...uint64) map[uint64]interface{} {
-	res := make(map[uint64]interface{}, len(keys))
-	c.mu.RLock()
-	for _, key := range keys {
-		idx, elem, exists := c.get(key)
-		if exists {
-			if notStale {
-				expired := elem.expires > 0 && elem.expires < nowNano
-				if expired {
-					continue
-				}
-			}
-			res[key] = elem.value
-			c.promote(idx)
-		}
-	}
-	c.mu.RUnlock()
-	return res
-}
-
-func (c *Cache) MGetString(keys ...string) map[string]interface{} {
-	nowNano := time.Now().UnixNano()
-	return c.mgetString(false, nowNano, keys...)
-}
-
-func (c *Cache) MGetStringNotStale(keys ...string) map[string]interface{} {
-	nowNano := time.Now().UnixNano()
-	return c.mgetString(true, nowNano, keys...)
-}
-
-func (c *Cache) mgetString(notStale bool, nowNano int64, keys ...string) map[string]interface{} {
-	res := make(map[string]interface{}, len(keys))
-	c.mu.RLock()
-	for _, key := range keys {
-		idx, elem, exists := c.get(key)
-		if exists {
-			if notStale {
-				expired := elem.expires > 0 && elem.expires < nowNano
-				if expired {
-					continue
-				}
-			}
-			res[key] = elem.value
-			c.promote(idx)
-		}
-	}
-	c.mu.RUnlock()
-	return res
-}
-
-func (c *Cache) promote(idx uint32) {
+func (c *Cache[K, V]) promote(idx uint32) {
 	buf := (*walbuf)(atomic.LoadPointer(&c.buf))
 	i := atomic.AddInt32(&buf.p, 1)
 	if i <= walBufSize {
@@ -302,7 +253,7 @@ func (c *Cache) promote(idx uint32) {
 	}
 
 	// the oldbuf has been swapped, we take responsibility to flush it
-	go func(c *Cache, buf *walbuf) {
+	go func(c *Cache[K, V], buf *walbuf) {
 		c.mu.Lock()
 		c.flushBuf(buf)
 		c.mu.Unlock()
@@ -310,125 +261,14 @@ func (c *Cache) promote(idx uint32) {
 	}(c, oldbuf)
 }
 
-func (c *Cache) Set(key, value interface{}, ttl time.Duration) {
-	var expires int64
-	if ttl > 0 {
-		expires = time.Now().UnixNano() + int64(ttl)
-	}
-	c.mu.Lock()
-	c.checkAndFlushBuf()
-	c.set(key, value, expires)
-	c.mu.Unlock()
-}
-
-func (c *Cache) MSet(kvmap interface{}, ttl time.Duration) {
-	var expires int64
-	if ttl > 0 {
-		expires = time.Now().UnixNano() + int64(ttl)
-	}
-	m := reflect.ValueOf(kvmap)
-	keys := m.MapKeys()
-
-	c.mu.Lock()
-	c.checkAndFlushBuf()
-	for _, key := range keys {
-		value := m.MapIndex(key)
-		c.set(key.Interface(), value.Interface(), expires)
-	}
-	c.mu.Unlock()
-}
-
-func (c *Cache) set(k, v interface{}, expires int64) {
-	idx, exists := c.m[k]
-	if exists {
-		e := c.list.get(idx)
-		e.value = v
-		e.expires = expires
-		c.list.MoveToFront(e)
-	} else {
-		e := c.list.Back()
-		if e.key != nil {
-			delete(c.m, e.key)
-		}
-		e.key = k
-		e.value = v
-		e.expires = expires
-		c.m[k] = e.index
-		c.list.MoveToFront(e)
-	}
-}
-
-func (c *Cache) Del(key interface{}) {
-	c.mu.Lock()
-	c.checkAndFlushBuf()
-	c.del(key)
-	c.mu.Unlock()
-}
-
-func (c *Cache) MDel(keys ...interface{}) {
-	c.mu.Lock()
-	c.checkAndFlushBuf()
-	for _, key := range keys {
-		c.del(key)
-	}
-	c.mu.Unlock()
-}
-
-func (c *Cache) MDelInt(keys ...int) {
-	c.mu.Lock()
-	c.checkAndFlushBuf()
-	for _, key := range keys {
-		c.del(key)
-	}
-	c.mu.Unlock()
-}
-
-func (c *Cache) MDelInt64(keys ...int64) {
-	c.mu.Lock()
-	c.checkAndFlushBuf()
-	for _, key := range keys {
-		c.del(key)
-	}
-	c.mu.Unlock()
-}
-
-func (c *Cache) MDelUint64(keys ...uint64) {
-	c.mu.Lock()
-	c.checkAndFlushBuf()
-	for _, key := range keys {
-		c.del(key)
-	}
-	c.mu.Unlock()
-}
-
-func (c *Cache) MDelString(keys ...string) {
-	c.mu.Lock()
-	c.checkAndFlushBuf()
-	for _, key := range keys {
-		c.del(key)
-	}
-	c.mu.Unlock()
-}
-
-func (c *Cache) del(key interface{}) {
-	idx, exists := c.m[key]
-	if exists {
-		delete(c.m, key)
-		elem := c.list.get(idx)
-		elem.key = nil
-		elem.value = nil
-		c.list.MoveToBack(elem)
-	}
-}
-
-func (c *Cache) checkAndFlushBuf() {
+func (c *Cache[K, V]) checkAndFlushBuf() {
 	buf := (*walbuf)(c.buf)
 	if buf.p > 0 {
 		c.flushBuf(buf)
 	}
 }
 
-func (c *Cache) flushBuf(buf *walbuf) {
+func (c *Cache[K, V]) flushBuf(buf *walbuf) {
 	if buf.p == 0 {
 		return
 	}
