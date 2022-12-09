0596a31af5a7c46a8be9d8d6acd7b71e00a1b5ee
jxskiss <jxskiss@126.com>
2022-12-09 22:08:22 +0800

    new api easy.Glob supporting double-star pattern
    Change-Id: Ifc8817a7fb3ae0e38b7a7f41ad659d8a8c9dc01e

---
+++ easy/filepath.go
@@ -0,0 +1,59 @@
+package easy
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Glob adds double-star support to the std library's [path/filepath.Glob].
+// It's useful when your pattern might have double-stars.
+func Glob(pattern string) (matches []string, err error) {
+	if !strings.Contains(pattern, "**") {
+		// Pass-through to std lib if no double-star.
+		return filepath.Glob(pattern)
+	}
+	parts := strings.Split(pattern, "**")
+	return globParts(parts).Expand()
+}
+
+type globParts []string
+
+func (globs globParts) Expand() (matches []string, err error) {
+	matches = []string{""}
+	for i, glob := range globs {
+		isLast := i == len(globs)-1
+		var hits []string
+		var seen = make(map[string]bool)
+		for _, match := range matches {
+			paths, err := filepath.Glob(match + glob)
+			if err != nil {
+				return nil, err
+			}
+			for _, path := range paths {
+				err = filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+					if err != nil {
+						return err
+					}
+					if isLast || info.IsDir() {
+						if _, ok := seen[path]; !ok {
+							hits = append(hits, path)
+							seen[path] = true
+						}
+					}
+					return nil
+				})
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
+		matches = hits
+	}
+
+	if len(globs) == 0 && len(matches) > 0 && matches[0] == "" {
+		matches = matches[1:]
+	}
+
+	return matches, nil
+}
---
+++ easy/filepath_test.go
@@ -0,0 +1,86 @@
+package easy
+
+import (
+	"os"
+	"testing"
+)
+
+func makeGlobTestDir(t *testing.T) {
+	t.Helper()
+	path := "./testdata/a/b/c.d/e.f"
+	err := os.MkdirAll(path, 0755)
+	if err != nil {
+		t.Fatalf("os.MkdirAll: %v", err)
+	}
+}
+
+func removeGlobTestDir() {
+	_ = os.RemoveAll("./testdata/a")
+}
+
+func TestGlob(t *testing.T) {
+	makeGlobTestDir(t)
+	defer removeGlobTestDir()
+
+	// test passthru to vanilla path/filepath
+	{
+		matches, err := Glob("./*/*/*/*.d")
+		if err != nil {
+			t.Fatalf("Glob: %s", err)
+		}
+		if len(matches) != 1 {
+			t.Fatalf("got %d matches, expected 1", len(matches))
+		}
+		expected := "testdata/a/b/c.d"
+		if matches[0] != expected {
+			t.Fatalf("matched [%s], expected [%s]", matches[0], expected)
+		}
+	}
+
+	// test a single double-star
+	{
+		matches, err := Glob("./**/*.f")
+		if err != nil {
+			t.Fatalf("Glob: %s", err)
+		}
+		if len(matches) != 1 {
+			t.Fatalf("got %d matches, expected 1", len(matches))
+		}
+		expected := "testdata/a/b/c.d/e.f"
+		if matches[0] != expected {
+			t.Fatalf("matched [%s], expected [%s]", matches[0], expected)
+		}
+	}
+
+	// test a single double-star
+	{
+		matches, err := Glob("./testdata/**/*.*")
+		if err != nil {
+			t.Fatalf("Glob: %s", err)
+		}
+		if len(matches) != 2 {
+			t.Fatalf("got %d matches, expected 2", len(matches))
+		}
+		expected := []string{"testdata/a/b/c.d", "testdata/a/b/c.d/e.f"}
+		for i, match := range matches {
+			if match != expected[i] {
+				t.Fatalf("matched [%s], expected [%s]", match, expected[i])
+			}
+		}
+	}
+
+	// test two double-stars
+	{
+		matches, err := Glob("./**/b/**/*.f")
+		if err != nil {
+			t.Fatalf("Glob: %s", err)
+		}
+		if len(matches) != 1 {
+			t.Fatalf("got %d matches, expected 1", len(matches))
+		}
+		expected := "testdata/a/b/c.d/e.f"
+		if matches[0] != expected {
+			t.Fatalf("matched [%s], expected [%s]", matches[0], expected)
+		}
+	}
+}
