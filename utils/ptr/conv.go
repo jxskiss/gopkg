35b184e526a1f141e7d6410bd9ea6eb00a69c42b
jxskiss <jxskiss@126.com>
2023-03-21 19:17:17 +0800

    Fix unsigned integer conversion
    Change-Id: Ia9e19b2cc9679fa6e219187d1288c4c0c5ee69e4

--- utils/ptr/conv.go
+++ utils/ptr/conv.go
@@ -1,6 +1,7 @@
 package ptr
 
 import (
+	"reflect"
 	"strconv"
 
 	"github.com/jxskiss/gopkg/v2/internal/constraints"
@@ -8,8 +9,16 @@ import (
 
 // IntToStringp converts Integer x to a string pointer.
 func IntToStringp[T constraints.Integer](x T) *string {
-	str := strconv.FormatInt(int64(x), 10)
-	return &str
+	rv := reflect.ValueOf(x)
+	switch rv.Kind() {
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
+		s := strconv.FormatInt(rv.Int(), 10)
+		return &s
+	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint, reflect.Uintptr:
+		s := strconv.FormatUint(rv.Uint(), 10)
+		return &s
+	}
+	return nil // unreachable
 }
 
 // IntpToStringp converts x to a string pointer.
@@ -18,8 +27,16 @@ func IntpToStringp[T constraints.Integer](x *T) *string {
 	if x == nil {
 		return nil
 	}
-	str := strconv.FormatInt(int64(*x), 10)
-	return &str
+	rv := reflect.ValueOf(*x)
+	switch rv.Kind() {
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
+		s := strconv.FormatInt(rv.Int(), 10)
+		return &s
+	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint, reflect.Uintptr:
+		s := strconv.FormatUint(rv.Uint(), 10)
+		return &s
+	}
+	return nil // unreachable
 }
 
 // IntpToString converts x to a string.
@@ -28,5 +45,14 @@ func IntpToString[T constraints.Integer](x *T) string {
 	if x == nil {
 		return ""
 	}
-	return strconv.FormatInt(int64(*x), 10)
+	rv := reflect.ValueOf(*x)
+	switch rv.Kind() {
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
+		s := strconv.FormatInt(rv.Int(), 10)
+		return s
+	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint, reflect.Uintptr:
+		s := strconv.FormatUint(rv.Uint(), 10)
+		return s
+	}
+	return "" // unreachable
 }
--- utils/ptr/conv_test.go
+++ utils/ptr/conv_test.go
@@ -1,12 +1,36 @@
 package ptr
 
 import (
+	"fmt"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestIntToStringp(t *testing.T) {
-	got := IntToStringp(1234)
-	assert.Equal(t, "1234", *got)
+	got1 := IntToStringp(1234)
+	assert.Equal(t, "1234", *got1)
+
+	want2 := fmt.Sprint(uint64(math.MaxUint64))
+	got2 := IntToStringp(uint64(math.MaxUint64))
+	assert.Equal(t, want2, *got2)
+}
+
+func TestIntpToStringp(t *testing.T) {
+	got1 := IntpToStringp(Int32(1234))
+	assert.Equal(t, "1234", *got1)
+
+	want2 := fmt.Sprint(uint64(math.MaxUint64))
+	got2 := IntpToStringp(Uint64(uint64(math.MaxUint64)))
+	assert.Equal(t, want2, *got2)
+}
+
+func TestIntpToString(t *testing.T) {
+	got1 := IntpToString(Int32(1234))
+	assert.Equal(t, "1234", got1)
+
+	want2 := fmt.Sprint(uint64(math.MaxUint64))
+	got2 := IntpToString(Uint64(uint64(math.MaxUint64)))
+	assert.Equal(t, want2, got2)
 }
