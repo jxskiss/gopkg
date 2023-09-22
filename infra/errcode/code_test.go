b3056dbf64065f3997881ee36731cde31c395263
jxskiss <jxskiss@126.com>
2023-09-22 11:04:48 +0800

    errcode improvements
    Change-Id: Iec9896b12e01f8b5aa6fba92f030814e5707e42b

--- infra/errcode/code_test.go
+++ infra/errcode/code_test.go
@@ -2,9 +2,11 @@ package errcode
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCode(t *testing.T) {
@@ -12,7 +14,7 @@ func TestCode(t *testing.T) {
 	dummy1 := reg.Register(100001, "")
 	dummy2 := reg.RegisterReserved(100002, "dummy2")
 
-	assert.Equal(t, dummy1.Error(), "[100001] (no message)")
+	assert.Equal(t, dummy1.Error(), "[100001] unknown")
 	assert.Equal(t, dummy2.Error(), "[100002] dummy2")
 
 	got1 := func() error { return dummy1 }()
@@ -31,6 +33,72 @@ func TestCode(t *testing.T) {
 	json2, _ := dummy2.MarshalJSON()
 	assert.Equal(t, []byte(`{"code":100001}`), json1)
 	assert.Equal(t, []byte(`{"code":100002,"message":"dummy2"}`), json2)
+}
+
+func TestCodeFormat(t *testing.T) {
+	reg := New()
+	badReqErr := reg.Register(100400, "bad request")
+
+	t.Run("no details", func(t *testing.T) {
+		err := func() error {
+			return badReqErr
+		}()
+
+		got1 := fmt.Sprintf("%v", err)
+		assert.Equal(t, "[100400] bad request", got1)
+
+		got2 := fmt.Sprintf("%+v", err)
+		assert.Equal(t, "[100400] bad request", got2)
+	})
+
+	t.Run("with details", func(t *testing.T) {
+		err := func() error {
+			return badReqErr.AddDetails("test detail", 12345)
+		}()
+
+		got1 := fmt.Sprintf("%v", err)
+		assert.Equal(t, "[100400] bad request", got1)
+
+		got2 := fmt.Sprintf("%+v", err)
+		t.Log(got2)
+		assert.Equal(t, "[100400] bad request\ndetails:\n -  test detail\n -  12345", got2)
+	})
+}
+
+func TestDetails(t *testing.T) {
+	reg := New()
+	testErr := reg.Register(1001, "test error")
+
+	t.Run("not ErrCode", func(t *testing.T) {
+		err := errors.New("not an ErrCode")
+		got := Details(err)
+		assert.Nil(t, got)
+	})
+
+	t.Run("no details", func(t *testing.T) {
+		err := testErr
+		got := Details(err)
+		assert.Nil(t, got)
+	})
+
+	t.Run("with details", func(t *testing.T) {
+		err := testErr.AddDetails("test detail", 12345)
+		got := Details(err)
+		require.Len(t, got, 2)
+		assert.Equal(t, "test detail", got[0])
+		assert.Equal(t, 12345, got[1])
+	})
+
+	t.Run("wrapped", func(t *testing.T) {
+		err := func() error {
+			err1 := testErr.AddDetails("test detail", 12345)
+			return fmt.Errorf("wrap message: %w", err1)
+		}()
+		got := Details(err)
+		require.Len(t, got, 2)
+		assert.Equal(t, "test detail", got[0])
+		assert.Equal(t, 12345, got[1])
+	})
 }
 
 func TestIs(t *testing.T) {
